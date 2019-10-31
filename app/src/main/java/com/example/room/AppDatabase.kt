900533a12eb12a7dd06665641f8cbef8753f1f07
Jacob Hearst <jhearst@protonmail.ch>
2019-10-31 13:36:39 -0500

    Create initial database structure

---
+++ app/src/main/java/com/example/room/AppDatabase.kt
@@ -0,0 +1,30 @@
+package com.example.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Workout::class, Interval::class], version = 1, exportSchema = false)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun workoutDao(): WorkoutDao
+    abstract fun intervalDao(): IntervalDao
+
+    /**
+     * Singleton pattern to avoid building the database multiple times
+     */
+    companion object {
+        private var instance: AppDatabase? = null
+        fun getInstance(context: Context): AppDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context,
+                    AppDatabase::class.java,
+                    "app_db"
+                ).build()
+            }
+
+            return instance as AppDatabase
+        }
+    }
+}
