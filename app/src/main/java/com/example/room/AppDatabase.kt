5217f3f2670137b2b630f09bab81dc73aefc8b78
Jacob Hearst <jhearst@protonmail.ch>
2019-11-01 12:41:37 -0500

    Add documentation to AppDatabase

--- app/src/main/java/com/example/room/AppDatabase.kt
+++ app/src/main/java/com/example/room/AppDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
+/**
+ * Database for storing workouts and intervals
+ *
+ */
 @Database(entities = [Workout::class, Interval::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun workoutDao(): WorkoutDao
