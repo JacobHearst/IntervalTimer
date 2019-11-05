e90458c1112372d0d2424d09e883969539e7f434
Jacob <8368015+JacobHearst@users.noreply.github.com>
2019-11-05 10:15:32 -0600

    Add framework for navigation (#8)

--- app/src/main/java/com/example/intervaltimer/MainActivity.kt
+++ app/src/main/java/com/example/intervaltimer/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.intervaltimer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize up navigation
+        val navController = this.findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    /**
+     * Support upwards navigation
+     */
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
     }
 }
