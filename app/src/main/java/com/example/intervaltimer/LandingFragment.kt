cd4fefa1f022d5738bf1030c4114c7408104e0f9
Jay Williams <williamsm7988@my.uwstout.edu>
2019-12-03 10:10:38 -0600

    Swipe now works on landing page

--- app/src/main/java/com/example/intervaltimer/LandingFragment.kt
+++ app/src/main/java/com/example/intervaltimer/LandingFragment.kt
@@ -3,7 +3,9 @@ package com.example.intervaltimer
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
@@ -18,6 +20,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.ItemTouchHelper
 import com.example.viewmodel.WorkoutViewModel
+import kotlin.math.*
 
 /**
  * The home screen fragment for the app. This fragment shows the user all available workouts,
@@ -43,10 +46,43 @@ class LandingFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_landing, container, false)
 
-        val floatingActionButton = rootView.addWorkoutButton//rootView.findViewById<FloatingActionButton>(R.id.addWorkoutButton)
+        // Needed variables for the swiping
+        val MIN_SWIPE_DISTANCE = 300;  // Minimum distance to activate swipe
+        val MAX_ANGLE = +30 // Max angle swipe
+        val MIN_ANGLE = -30 // Min angle swipe
+        var startX = 0f
+        var endX = 0f
+        var startY = 0f
+        var endY = 0f
+
+        rootView.cardRecyclerView?.setOnTouchListener(object: View.OnTouchListener {
+            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+                if (event?.action == MotionEvent.ACTION_DOWN) {
+                    startX = event.x
+                    startY = event.y
+                }
+                if (event?.action == MotionEvent.ACTION_UP) {
+                    endX = event.x
+                    endY = event.y
+                    var deltaX = startX - endX
+                    var deltaY = startY - endY
+                    var swipeDistance = abs(sqrt(deltaX.pow(2)+ deltaY.pow(2)))
+                    var angle = atan2(deltaY, deltaX) * 180 / PI
+                    if (swipeDistance >= MIN_SWIPE_DISTANCE && angle <= MAX_ANGLE &&
+                            angle >= MIN_ANGLE) {
+                        Toast.makeText(activity, "Swipe right to left", Toast.LENGTH_LONG).show()
+                    }
+                }
+                return true
+            }
+        })
+
+        val floatingActionButton =
+            rootView.addWorkoutButton//rootView.findViewById<FloatingActionButton>(R.id.addWorkoutButton)
 
         floatingActionButton?.setOnClickListener {
             val dialog = AddWorkoutModalFragment()
@@ -75,5 +111,6 @@ class LandingFragment : Fragment() {
 
         return rootView
     }
+
 
 }
