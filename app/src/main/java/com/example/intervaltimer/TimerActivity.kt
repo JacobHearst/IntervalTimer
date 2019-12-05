25d4f6e0032518d5b79756ec341ffc8caa97cc2d
Smrecek <smrecekb0004@my.uwstout.edu>
2019-12-05 10:20:18 -0600

    Modified the timer screen

--- app/src/main/java/com/example/intervaltimer/TimerActivity.kt
+++ app/src/main/java/com/example/intervaltimer/TimerActivity.kt
@@ -1,18 +1,23 @@
 package com.example.intervaltimer
 
+import android.graphics.Color
 import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.navArgs
 import kotlinx.android.synthetic.main.activity_timer.*
 
 class TimerActivity : AppCompatActivity() {
 
     private var mediaPlayer: MediaPlayer? = null
     private var buttonSwitch = false
-    private var totaltime: Long = 10000 + 1000
+    private var totaltime: Long = 0
     private val countdown: Long = 1000
     private var ctimer: CountDownTimer? = null
+    private var itera: Int = 0
+
+    private val args: TimerActivityArgs by navArgs()
 
     fun playTimer(){
 
@@ -31,7 +36,7 @@ class TimerActivity : AppCompatActivity() {
 
 
             override fun onFinish(){
-                mediaPlayer?.start()
+                resetTimer()
             }
         }
         ctimer?.start()
@@ -49,12 +54,29 @@ class TimerActivity : AppCompatActivity() {
 
         invalidateOptionsMenu()
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.bellring)
+        workoutName.text = args.intervalList.List[0].name // placeholder code for setting text at startup
 
-        workoutName.text = "Push ups" // placeholder code for setting text at startup
-        repsText.text = "# of reps 50" // placeholder code for setting text at startup
+        if (args.intervalList.List[0].reps.toString() != "null"){
+            repsText.text = args.intervalList.List[0].reps.toString() // placeholder code for setting text at startup
+        }
 
-        timerText.text = "00:10" // placeholder code for setting text at startup
+        var covertTime: Int?
+        covertTime = args.intervalList.List[0].time
+
+        var minutes = covertTime!! / 60
+        var seconds = covertTime % 60
+
+
+        val covertTimeText = String.format("%02d:%02d", minutes, seconds)
+
+        totaltime = (minutes * 60000 + seconds * 1000 + 1000).toLong()
+
+        timerText.text = covertTimeText
+
+        timerText.setTextColor(Color.parseColor(args.intervalList.List[0].color))
+        repsText.setTextColor(Color.parseColor(args.intervalList.List[0].color))
+        workoutName.setTextColor(Color.parseColor(args.intervalList.List[0].color))
+        workoutProgressBar.setBackgroundColor(Color.parseColor(args.intervalList.List[0].color))
 
         playButton.setOnClickListener{
 
@@ -73,30 +95,17 @@ class TimerActivity : AppCompatActivity() {
         }
 
         fastForwardButton.setOnClickListener{
-            mediaPlayer?.stop()
-            totaltime = 70000 + 1000
-            workoutName.text = "Curls" // placeholder code for setting text when hitting forward
-            repsText.text = "# of reps 80" // placeholder code for setting text when hitting forward
-            timerText.text = "01:10" // placeholder code for setting text at startup
-            playButton.setBackgroundResource(R.drawable.ic_play)
-            pause()
-            buttonSwitch = false
+            forwardbackword(1)
         }
 
         rewindButton.setOnClickListener{
-            mediaPlayer?.stop()
-            totaltime = 10000 + 1000
-            workoutName.text = "Push ups" // placeholder code for setting text when hitting back
-            repsText.text = "# of reps 50" // placeholder code for setting text when hitting back
-            timerText.text = "00:10" // placeholder code for setting text at startup
-            playButton.setBackgroundResource(R.drawable.ic_play)
-            pause()
-            buttonSwitch = false
+            forwardbackword(-1)
         }
 
         endWorkoutButton.setOnClickListener{
             mediaPlayer?.release()
             mediaPlayer = null
+            finish()
         }
 
 
@@ -104,8 +113,58 @@ class TimerActivity : AppCompatActivity() {
 
     }
 
+    fun forwardbackword(nav: Int){
+
+        if (itera == 0 && nav == -1){
+
+        } else if(itera == (args.intervalList.List.size - 1)){
+
+        }else{
+            itera += nav
+        }
+
+        workoutName.text = args.intervalList.List[itera].name // placeholder code for setting text at startup
+
+        if (args.intervalList.List[0].reps.toString() != "null"){
+            repsText.text = args.intervalList.List[itera].reps.toString() // placeholder code for setting text at startup
+        }
+
+        var covertTime: Int?
+        covertTime = args.intervalList.List[itera].time
+
+        var minutes = covertTime!! / 60
+        var seconds = covertTime % 60
+
+
+        val covertTimeText = String.format("%02d:%02d", minutes, seconds)
+
+        totaltime = (minutes * 60000 + seconds * 1000 + 1000).toLong()
+
+        timerText.text = covertTimeText
+
+        timerText.setTextColor(Color.parseColor(args.intervalList.List[itera].color))
+        repsText.setTextColor(Color.parseColor(args.intervalList.List[itera].color))
+        workoutName.setTextColor(Color.parseColor(args.intervalList.List[itera].color))
+        workoutProgressBar.setBackgroundColor(Color.parseColor(args.intervalList.List[itera].color))
+
+//        mediaPlayer?.stop()
+//        totaltime = 70000 + 1000
+//        workoutName.text = "Curls" // placeholder code for setting text when hitting forward
+//        repsText.text = "# of reps 80" // placeholder code for setting text when hitting forward
+//        timerText.text = "01:10" // placeholder code for setting text at startup
+        playButton.setBackgroundResource(R.drawable.ic_play)
+        pause()
+        buttonSwitch = false
+
+    }
+
     override fun onSupportNavigateUp(): Boolean {
-        //findNavController().navigate()
+        finish()
         return false
+    }
+
+    fun resetTimer(){
+        mediaPlayer = MediaPlayer.create(this, R.raw.bellring)
+        mediaPlayer?.start()
     }
 }
